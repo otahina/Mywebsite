8c8914046444aee15fb8c0d2eba3852ac6c433d4
otahina <uking1066hina@gmail.com>
2023-09-13 09:14:21 +0200

    update

--- js/script.js
+++ js/script.js
@@ -54,3 +54,36 @@ buttonIds.forEach(buttonId => {
     box.classList.remove('hover');
   });
 });
+
+// Contact form
+const contactForm = document.querySelector('#contact-form');
+let name = document.getElementById('name');
+let email = document.getElementById('email');
+let message = document.getElementById('message');
+
+contactForm.addEventListener('submit', (e)=> { 
+    e.preventDefault();
+    
+    let formData = {
+      name: name.value,
+      email: email.value,
+      message: message.value
+    }
+    
+    let xhr = new XMLHttpRequest();
+    xhr.open('POST', '/');
+    xhr.setRequestHeader('content-type', 'application/json');
+    xhr.onload = function() {
+      console.log(xhr.responseText);
+      if(xhr.responseText == 'success') {
+        alert('Email sent');
+        name.value = '';
+        email.value = '';
+        message.value = '';
+      } else {
+        alert('Something went wrong');
+      }
+    }
+
+    xhr.send(JSON.stringify(formData));
+});
--- server.js
+++ server.js
@@ -1,11 +1,46 @@
 const express = require('express');
+const nodemailer = require('nodemailer');
+const path = require('path');
 const app = express();
 
 const port = process.env.PORT || 5001;
 
+app.use(express.static(__dirname));
+app.use(express.json());
+
+require('dotenv').config();
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/../index.html');
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.post('/', (req, res) => {
+    console.log(req.body);
+
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: process.env.GMAIL_USER,
+            pass: process.env.GMAIL_PASS
+        }
+    });
+
+    const mailOptions = {
+        from: req.body.email,
+        to: 'uking1066hina@gmail.com',
+        subject: `Message from ${req.body.email}: ${req.body.name}`,
+        text: req.body.message
+    }
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if(error) {
+            console.log(error);
+            res.send('error');
+        } else {
+            console.log('Email sent: ' + info.response);
+            res.send('success');
+        }
+    });
 });
 
 app.listen(port, () => {
